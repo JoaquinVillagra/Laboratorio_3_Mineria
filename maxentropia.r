418af06097da7af822b60fd29439997f0bcd406f
Joaquín Villagra <joaquin.villagra@usach.cl>
2017-09-23 11:12:05 -0300

    Carga libreria

---
+++ maxentropia.r
@@ -0,0 +1,7 @@
+#Laboratorio 3 Minería de Datos Avanzada
+#Ignacio Ibañez Aliaga
+#Joaquín Villagra Pacheco
+
+#Importando datos
+require(maxent)
+data <- read.csv(system.file("data/NYTimes.csv.gz",package="maxent"))
