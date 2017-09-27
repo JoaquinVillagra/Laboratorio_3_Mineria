d3dfdec0d5cfe1c665cb18da77f35865aecc0141
Joaquín Villagra <joaquin.villagra@usach.cl>
2017-09-27 06:14:40 -0300

    Agrega quitar_StopWords de Archivo

--- maxentropia.r
+++ maxentropia.r
@@ -11,21 +11,16 @@ require(maxent)
 data <- read.csv(system.file("data/NYTimes.csv.gz",package="maxent"))
 
 #se obtiene una estructura con metadatos obtenidos de los titulos de los 3104 documentos del NY y sus frecuencias de aparici??n en el documento.
-corpus_p <- Corpus(VectorSource(data$Title[1:1500]))
+corpus_p <- Corpus(VectorSource(data$Title[1:3104]))
 
 ##############  Datos sin preprocesamiento
-corpus[[1]]$content
-for (i in 1:10) print (corpus[[i]]$content) 
-
-dtm1 <- DocumentTermMatrix(corpus) 
-dtm1.data <- as.matrix(dtm1)
+corpus_p[[1]]$content
+for (i in 1:10) print (corpus_p[[i]]$content) 
 
 matrix1 <- DocumentTermMatrix(corpus_p)
 sparse1 <- as.compressed.matrix(matrix1)
-f1 <- tune.maxent(sparse[1:1500,],data$Topic.Code[1:1500],nfold=3,showall=TRUE, verbose=TRUE)
+f1 <- tune.maxent(sparse1[1:2100,],data$Topic.Code[1:2100],nfold=3,showall=TRUE, verbose=TRUE)
 print(f1)
-model1  <- maxent(sparse1[1:1500,],data$Topic.Code[1:1500], l1_regularizer=0.2, l2_regularizer=0.0, use_sgd=FALSE, set_heldout=0, verbose=TRUE)
-results <- predict(model1,sparse1[100:120,])
 
-confusion_matrix <- confusionMatrixFor_Neg1_0_1(model1)
-statsFromConfusionMatrix(confusion_matrix)
+model1  <- maxent(sparse1[1:2100,],data$Topic.Code[1:2100], l1_regularizer=0.0, l2_regularizer=0.2, use_sgd=FALSE, set_heldout=0, verbose=TRUE)
+results <- predict(model1,sparse1[2200:2220,])
--- maxentropia_preprocesamiento.r
+++ maxentropia_preprocesamiento.r
@@ -11,24 +11,33 @@ require(maxent)
 data <- read.csv(system.file("data/NYTimes.csv.gz",package="maxent"))
 
 #se obtiene una estructura con metadatos obtenidos de los titulos de los 3104 documentos del NY y sus frecuencias de aparici??n en el documento.
-corpus_p <- Corpus(VectorSource(data$Title[1:1500]))
+corpus_p <- Corpus(VectorSource(data$Title[1:3104]))
+Summary(data[,5])
 
 ##############  Datos con preprocesamiento
 corpus_p <- tm_map(corpus_p, stripWhitespace)
 corpus_p <- tm_map(corpus_p, content_transformer(tolower))
-corpus_p <- tm_map(corpus, content_transformer(removeNumbers))
+corpus_p <- tm_map(corpus_p, content_transformer(removeNumbers))
 corpus_p <- tm_map(corpus_p, content_transformer(removePunctuation))
 corpus_p <- tm_map(corpus_p, content_transformer(removeWords), stopwords("english"))
 corpus_p <- tm_map(corpus_p, stemDocument, language="english")
-for (i in 1:10) print (corpus_p[[i]]$content) 
+
+# carga mi archivo de palabras vacías personalizada y lo convierte a ASCII
+sw <- readLines("/Volumes/HDD/Google Drive/Ingeniria civil Informatica - Uiversidad de Santiago de Chile/10mo Semestre/Minería de datos Avanzada/Laboratorios/Laboratorio_3_Mineria/list_stopwords_english.txt",encoding="UTF-8")
+sw = iconv(sw, to="ASCII//TRANSLIT")
+
+#for (i in 1:10) print (corpus_p[[i]]$content) 
 
 dtm <- DocumentTermMatrix(corpus_p) 
 dtm.data <- as.matrix(dtm)
 
 matrix <- DocumentTermMatrix(corpus_p)
 sparse <- as.compressed.matrix(matrix)
-f <- tune.maxent(sparse[1:1500,],data$Topic.Code[1:1500],nfold=3,showall=TRUE, verbose=TRUE)
+f <- tune.maxent(sparse[1:2100,],data$Topic.Code[1:2100],nfold=3,showall=TRUE, verbose=TRUE)
 print(f)
-model<-maxent(sparse[1:1500,],data$Topic.Code[1:1500], l1_regularizer=0.2, l2_regularizer=0.0, use_sgd=FALSE, set_heldout=0, verbose=TRUE)
-results <- predict(model,sparse[101:120,]) 
-confusionMatrixFor_Neg1_0_1(results)
+# accuracy            pct_best_fit
+# 0.5103830           1.0000000
+# l1_regularizer=0.0, l2_regularizer=0.4
+
+model<-maxent(sparse[1:2100,],data$Topic.Code[1:2100], l1_regularizer=0.0, l2_regularizer=0.4, use_sgd=FALSE, set_heldout=0, verbose=TRUE)
+results <- predict(model,sparse[2200:2220,]) 
